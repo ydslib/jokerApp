f8514d741c870dc062765f28e19e77181646d01a
dongsheng ye <yedongsheng@codemao.cn>
2024-04-22 10:57:53 +0800

    添加common模块以及统一配置文件

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,16 @@
+object Config {
+
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 33
+    const val compileSdk = 33
+    const val versionCode = 100
+    const val versionName = "1.0.0"
+
+
+    const val coreKtx = "1.9.0"
+    const val lifecycleRuntimeKtx = "2.2.0"
+    const val appcompat = "1.6.1"
+    const val material = "1.11.0"
+    const val LogLib = "1.0.0"
+    const val BaseRecyclerViewAdapterHelper4 = "4.1.4"
+}
